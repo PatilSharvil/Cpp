e0eca25428e7d11a8beb483e840ef4f3422bac29
Sharvil Sunil Patil <patilsharvil03@gmail.com>
2026-03-19 17:59:39 +0530

    map operations implementations

--- Day 11/Structured_Binding.cpp
+++ Day 11/Structured_Binding.cpp
@@ -11,5 +11,19 @@ int main(int argc, char** args) {
         cout << key << " " << value << endl;
     }
 
+    m.emplace(3, "Raghav");
+    cout << endl;
+    for (auto [key, value] : m) {
+        cout << key << " " << value << endl;
+    }
+
+    m.erase(3);
+    cout << endl;
+
+    for (auto [key, value] : m) {
+        cout << key << " " << value << endl;
+    }
+    cout << endl;
+
     return 0;
 }
